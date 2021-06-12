f77222245062f74e3f4d59dee5c17f9ea4b80279
Sander Latour <sander@sendwave.com>
2021-06-12 13:35:12 +0200

    Boilerplate heroku+django+wagtail

---
+++ schumanshow/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-3^^c!k$n-to&z-4civc1$8$7=278jz@5%it0#vu2gc+q9&_dxf'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
