89bbcc131defeef919d09d07a8682676ffd80513
Sander Latour <sander@sendwave.com>
2021-06-13 12:18:46 +0200

    S3 storage for media files

--- schumanshow/settings/production.py
+++ schumanshow/settings/production.py
@@ -6,5 +6,9 @@ from .base import *
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+AWS_ACCESS_KEY_ID = os.environ.get('BUCKETEER_AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('BUCKETEER_AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('BUCKETEER_BUCKET_NAME')
 django_heroku.settings(locals())
