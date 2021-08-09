aaadadd9132091448236c44c17fe839e6c0915b0
Sander Latour <sander@sendwave.com>
2021-08-09 15:27:40 +0200

    cast page

--- home/models.py
+++ home/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from wagtail.admin import blocks
 
 from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.images.blocks import ImageChooserBlock
 from wagtail.images.models import Image
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 
 
@@ -26,4 +28,25 @@ class HomePage(Page):
         FieldPanel('tagline'),
         FieldPanel('sub_tagline'),
         FieldPanel('body'),
+    ]
+
+class CastMemberBlock(blocks.StructBlock):
+    first_name = blocks.CharBlock()
+    surname = blocks.CharBlock()
+    photo = ImageChooserBlock(required=False)
+    description = blocks.RichTextBlock()
+    twitter = blocks.CharBlock()
+    linkedin = blocks.CharBlock()
+
+    class Meta:
+        icon = 'user'
+
+class CastPage(Page):
+    intro = RichTextField(default='')
+
+    cast_list = StreamField([('profile', CastMemberBlock())])
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro'),
+        StreamFieldPanel('cast_list'),
     ]
